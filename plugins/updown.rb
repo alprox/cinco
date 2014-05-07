12d0d9128d0387f35d0e01c7cf0ec2fe957d265f
alprox <chitzus@gmail.com>
2014-05-07 21:28:03 +0300

    snazzier isitup, json 'n all (json makes everything better)

--- plugins/updown.rb
+++ plugins/updown.rb
@@ -1,22 +1,48 @@
+# Original author: Richard Banks <namaste@rawrnet.net>
+# Modified by alprox 2014-05-07
+
 require 'cinch'
-require 'open-uri'
-require 'nokogiri'
+require 'ostruct'
+require 'open-uri/cached'
+require 'json'
 require 'cgi'
 
 class UpDown
   include Cinch::Plugin
-  plugin "updown"
-  help "!(up|down) <site> - check if a site is up or down for everyone"
+
+  set :plugin_name, 'updown'
+  set :help, "!(up|down) <domain> - check if a domain is up or down for everyone"
+
   match /(?:up|down) (.+)/
 
   def execute(m, query)
-    url = "http://downforeveryoneorjustme.com/#{CGI.escape(query)}"
-
-    doc = Nokogiri::HTML(open(url))
-    result = doc.at('div#container').text.strip.split(/\n/).first
-
-    m.reply result
+	return m.reply "we trippy mane" if check(query).nil?
+	updown_result(m, check(query))
   end
+
+  def check(terms)
+	url = JSON.parse(open("http://isitup.org/#{terms}.json").read)
+	OpenStruct.new(
+	domain: url['domain'],
+	status: url['status_code'],
+	time:	url['response_time']
+	)
+  rescue
+	nil
+  end
+
+  def updown_result(m, url)
+	if url.status == 3
+		m.reply "Not a valid domain!"
+	end
+
+	if url.status == 2
+		m.reply "#{url.domain} is down!"
+	end
+
+	if url.status == 1
+		m.reply "#{url.domain} is up! Response time: #{url.time}"
+	end
+  end
+
 end
-
-
