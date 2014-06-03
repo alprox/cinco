9cd1849d63cee757a2c18799775edf52c1193f5d
alprox <chitzus@gmail.com>
2014-06-04 01:14:21 +0300

    condense the memetic capacity

--- plugins/celery_man.rb
+++ plugins/celery_man.rb
@@ -35,14 +35,5 @@ class CeleryMan
     m.reply "Not computing. Please repeat:" if m.message.match /.*nude tayne/
     m.reply "http://i.imgur.com/yzLcf.png" if m.message.match /NUDE TAYNE/
 
-    m.reply "#{m.user.nick} oot homo" if m.message.match /!kurre/
-    m.reply "http://i.imgur.com/9W0PA.gif" if m.message.match /.*2spooky/
-    m.reply "http://i.imm.io/Tpza.gif" if m.message.match /discojytä/
-    m.reply "http://rakka.us.to/tuuhea.gif" if m.message.match /!tuuhea/
-    m.reply "http://i.imgur.com/xU6hA.gif" if m.message.match /.*tissit/
-    m.reply "I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop" if m.message.match /paska botti/
-    m.reply "mumble: es.rbt.asia:64420" if m.message.match /!mumble/
-    m.reply "radio: http://rakka.us.to:8000/stream" if m.message.match /!radio/
-
   end
 end
--- plugins/meemu.rb
+++ plugins/meemu.rb
@@ -20,11 +20,20 @@ class Meemu
 	match = switch_regex.match(m.message)
 	if match
 		m.reply kurre
+		m.reply "#{m.user.nick} oot homo"
 	end
 	switch_regex = /!iltis/
 	match = switch_regex.match(m.message)
 	if match
 		m.reply iltis
 	end
+
+	m.reply "http://i.imgur.com/9W0PA.gif" if m.message.match /.*2spooky/
+	m.reply "http://i.imm.io/Tpza.gif" if m.message.match /discojytä/
+	m.reply "http://rakka.us.to/tuuhea.gif" if m.message.match /!tuuhea/
+	m.reply "http://i.imgur.com/xU6hA.gif" if m.message.match /.*tissit/
+	m.reply "I feel offended by your recent action(s). Please read http://stop-irc-bullying.eu/stop" if m.message.match /paska botti/
+	m.reply "mumble: es.rbt.asia:64420" if m.message.match /!mumble/
+	m.reply "radio: http://rakka.us.to:8001/stream" if m.message.match /!radio/
   end
 end
