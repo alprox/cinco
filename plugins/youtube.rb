1a64108290d48f0e6b00ad16e11d95bdc8f4d036
alprox <chitzus@gmail.com>
2014-10-08 13:37:20 +0300

    forgot to :w

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -3,10 +3,11 @@ require 'youtube_it'
 class Youtube
   include Cinch::Plugin
 
-	match /.*(?:https?:\/\/)?(?:www\.)?youtu(?:\.be|be\.com)\/(?:watch\?v=)?(\S{10,}).*/ix, prefix:""
+	match /.*(?:youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=)([^#\&\?]*).*/ix, prefix:""
 	def execute(m,id)
 		client = YouTubeIt::Client.new
 		video = client.video_by(id)
-		m.reply Cinch::Formatting.format("%s: #{video.title} :: #{video.unique_id} :: #{video.author.name} :: Views: #{video.view_count}" % [Format(:bold, :red, 'Youtube')])
+		views = video.view_count.to_s.reverse.scan(/\d{3}|.+/).join(",").reverse # fancy
+		m.reply Cinch::Formatting.format("%s: #{video.title} :: #{video.unique_id} :: #{video.author.name} :: #{views}" % [Format(:bold, :red, 'Youtube')])
 	end
 end
