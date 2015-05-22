121fa07144231e8ac30bb31c1fc4a10e1ee91a59
alprox <chitzus@gmail.com>
2015-05-22 22:52:26 +0300

    update youtube.rb to use yt instead of youtube_it thank you google for making me get API keys for trivial stuff.

--- plugins/youtube.rb
+++ plugins/youtube.rb
@@ -1,14 +1,20 @@
-require 'youtube_it'
+require 'yt'
 
 class Youtube
   include Cinch::Plugin
 
-	match /.*(?:youtu.be\/|\/v\/|\/u\/\w\/|embed\/|watch\?v=)([a-zA-Z0-9_-]+)/ix, prefix:""
-	def execute(m,id)
-		client = YouTubeIt::Client.new
-		video = client.video_by(id)
-		views = video.view_count.to_s.reverse.scan(/\d{3}|.+/).join(",").reverse # fancy
-		duration = Time.at(video.duration).utc.strftime("%H:%M:%S") # wow whoa
-		m.reply Cinch::Formatting.format("%s: #{video.title} :: #{video.unique_id} :: #{video.author.name} :: #{views} :: #{duration}" % [Format(:bold, :red, 'Youtube')])
-	end
+    def initialize(*args)
+      super
+      Yt.configuration.api_key = Conf[:google][:api_key]
+    end
+
+    match /.*(?:youtu.be\/|\/v\/|\/u\/\w\/|embed\/|watch\?v=)([a-zA-Z0-9_-]+)/ix, prefix:""
+
+    def execute(m, video_id)
+      video = Yt::Video.new id: video_id
+      views = video.view_count.to_s.reverse.scan(/\d{3}|.+/).join(",").reverse
+      duration = Time.at(video.duration).utc.strftime("%H:%M:%S")
+      m.reply Cinch::Formatting.format("%s: #{video.title} :: #{video.id} :: #{video.channel_title} :: #{views} :: #{duration}" %
+              [Format(:bold, :red, 'Youtube')])
+      end
 end
